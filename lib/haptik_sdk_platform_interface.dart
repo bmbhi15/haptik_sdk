eebe7f87e81fc78e77bd28aba08a21497f52f58e
ushiwaka <anagh.awesome@gmail.com>
2022-12-14 11:23:19 +0530

    removing commented code

--- lib/haptik_sdk_platform_interface.dart
+++ lib/haptik_sdk_platform_interface.dart
@@ -10,20 +10,6 @@ abstract class HaptikSdkPlatform extends PlatformInterface {
 
   static final Object _token = Object();
 
-  // static HaptikSdkPlatform _instance = MethodChannelHaptikSdk();
-  //
-  // /// The default instance of [HaptikSdkPlatform] to use.
-  // ///
-  // /// Defaults to [MethodChannelHaptikSdk].
-  // static HaptikSdkPlatform get instance => _instance;
-  //
-  // /// Platform-specific implementations should set this with their own
-  // /// platform-specific class that extends [HaptikSdkPlatform] when
-  // /// they register themselves.
-  // static set instance(HaptikSdkPlatform instance) {
-  //   PlatformInterface.verifyToken(instance, _token);
-  //   _instance = instance;
-  // }
   @visibleForTesting
   static final methodChannel = const MethodChannel('haptik_sdk');
 
@@ -128,44 +114,4 @@ abstract class HaptikSdkPlatform extends PlatformInterface {
     final result= await methodChannel.invokeMethod<String>('setNotificationToken',data);
     return result;
   }
-  // Future<String?> launchGuestConversation(InitData initData) {
-  //   throw UnimplementedError('launchGuestConversation() has not been implemented.');
-  // }
-  // Future<String?> launchCustomSignupConversation(SignupData signupData) {
-  //   throw UnimplementedError('launchCustomSignupConversation() has not been implemented.');
-  // }
-  // Future<String?> logout() {
-  //   throw UnimplementedError('logout() has not been implemented.');
-  // }
-  // Future<String?> setLaunchMessage(String message, bool hidden, bool skipMessage)
-  // {
-  //   throw UnimplementedError('setLaunchMessage() has not been implemented.');
-  // }
-  // Future<String?> updateUserData(var userData)
-  // {
-  //   throw UnimplementedError('updateUserData() has not been implemented');
-  // }
-  // Future<String?> destroy()
-  // {
-  //   throw UnimplementedError('destroy() has not been implemented');
-  // }
-  // Future<String?> getFormattedNotificationText(var data)
-  // {
-  //   throw UnimplementedError('getFormattedNotificationText() has not been implemented');
-  // }
-  //
-  // Future<String?> handleNotification(var data)
-  // {
-  //   throw UnimplementedError('handleNotification() has not been implemented');
-  // }
-  //
-  // Future<bool?> isHaptikNotification(var data)
-  // {
-  //   throw UnimplementedError('isHaptikNotification() has not been implemented');
-  // }
-  //
-  // Future<String?> setNotificationToken(String data)
-  // {
-  //   throw UnimplementedError('setNotificationToken() has not been implemented');
-  // }
 }
